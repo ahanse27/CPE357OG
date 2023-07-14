3716fdf2811b7001d11d2bbbb88edce4bd0a569f
ahanse27 <91102191+ahanse27@users.noreply.github.com>
2023-07-13 19:40:08 -0700

    Add files via upload

---
+++ Project1/userInput.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/*
+    This will be the main for the entire program.
+    The user will input the functionality of how they would like to view the information.
+
+
+    The given option will be:
+        2014 population of entries in the file (pop.ops)
+        2014 pop of each field (pop_field.ops)...will need to use the percentages against total pop
+        2014 percentage of each field (percent_fields.ops)
+        filter receords by state (filter_state.ops)
+        2014 state total fields (total_state.ops)
+        filter by high school less than or equal toa percentage (high_school_le_60.ops)
+        filter by bachelors degree greater than or equal to a percentage (bachelors_ge_60.ops)
+
+
+    User will enter the given operation in a command line arg. Will compare strings (lower and upper case) to perform the op.
+    
+*/
+
+int main(int argc, char* argv){
+    
+
+    return 0;
+}
